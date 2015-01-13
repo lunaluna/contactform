d92bee61b5f45d7f469e5e41794173cd7f2478fa
Hiroki Saiki <web@lunalunadesign.com>
2015-01-13 12:35:07 +0900

    Create contact_thanks.php

---
+++ contact_thanks.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<meta name="keywords" content="">
+<meta name="description" content="">
+
+<link rel="stylesheet" href="./style.css" media="screen">
+
+<title>お問い合わせ完了画面 | 問い合わせフォームサンプル</title>
+</head>
+<body class="contact">
+	<div class="wrapper">
+		<div class="header">
+			<h1 class="page_title">問い合わせフォームサンプル</h1>
+			<div class="header_area clear-fix">
+				<h2 class="header_logo"><a href="./index.php">問い合わせフォームサンプル</a></h2>
+			</div>
+		</div>
+		<ul class="breadclumb">
+			<li><a href="./index.php">TOP</a></li>
+			<li>お問い合わせ完了画面</li>
+		</ul>
+		<div class="mainarea">
+			<div class="section contents_contact">
+				<div class="contact_innercontents">
+					<p class="mb_20"><strong>ご入力いただきましてありがとうございました。</strong><br />お問い合わせを送信致しました。</p>
+					<p class="mb_20">折り返し、ご登録いただいたメールアドレス宛てに<br>確認メールが届きます。<br />また、お送りいただきましたお問い合わせには、<br />数日中に弊社の担当者よりご連絡させていただきます。</p>
+					<p class="mb_20">お急ぎの方は下記までご連絡ください。<br /><strong>TEL: 03-xxxx-xxxx<br>FAX: 03-xxxx-xxxx</strong></p>
+					<p class="top_linkbutton">
+						<a href="./index.php"><button>トップページへ戻る</button></a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer">
+		<p class="copyright">Copyright 2015 &copy; form-sample All rights reserved.</p>
+	</div>
+</body>
+</html>
