f36d00b1b4a62c23a984778fc0940535cd4ab3c0
Hiroki Saiki <saiki@ace-union.net>
2015-01-22 20:16:22 +0900

    add no-index, no-follow

--- contact_confirm.php
+++ contact_confirm.php
@@ -14,6 +14,7 @@ $_SESSION = $_POST;
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="robots" content="noindex, nofollow">
 
 <meta name="keywords" content="">
 <meta name="description" content="">
